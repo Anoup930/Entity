b85736b22c4260b168b4b260bacc17dbd4c9a495
Anoup930 <66896484+Anoup930@users.noreply.github.com>
2022-06-19 14:00:58 +0600

    CRUD using Entity Framework

---
+++ StudentApi/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentApi.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Student> Students { get; set; }
+    }
+}
